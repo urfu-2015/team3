373d68a3f087549f8e7647439b3b2bf3d420933b
isukhachev <lifeandfre@gmail.com>
2016-03-13 17:27:17 +0500

    add register and autorizate

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const passport = require('./lib/passport');
 
 const morgan = require('morgan');
 
@@ -10,6 +11,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(morgan('dev'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({extended: true}));
+app.use(require('express-session')({
+    secret: 'kafkatist',
+    resave: false,
+    saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('port', (process.env.PORT || 5000));
 
--- controllers/pages.js
+++ controllers/pages.js
@@ -1,8 +1,9 @@
 // const url = 'mongodb://Anna.Smith:cr5jhj7hfccd15tn@ds064718.mlab.com:64718/kafkatist';
 
+const _ = require('lodash');
 exports.index = (req, res) => {
     res.render('index', Object.assign({
-        message: 'Hello, User2!'
+        message: `Hello, ${_.get(req, 'user.login', 'User')}!`
     }, req.commonData));
 };
 
