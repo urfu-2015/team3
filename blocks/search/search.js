d25cb95f9ede14fb93946e3661ee815a47331a7b
bazhenova <anyusha2595@gmail.com>
2016-03-27 16:19:45 +0500

    update 2 searcher

---
+++ blocks/search/search.js
@@ -0,0 +1,11 @@
+$("#search").on("input", function () {
+    $.ajax({
+        type: "GET",
+        url: "/search-word",
+        data: { word: this.value },
+        success: function(msg){
+            console.log("Данные:" + msg);
+        }
+    });
+});
+
