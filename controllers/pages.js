b82d89356dccdb89ccbef8ee6e1ba0506c647649
Екатерина Онуфриенко < >
2016-03-27 15:15:26 +0500

    anth

--- controllers/pages.js
+++ controllers/pages.js
@@ -14,6 +14,12 @@ exports.index = (req, res) => {
             'banana'
         ]
     }, req.commonData)));
+
+const _ = require('lodash');
+exports.index = (req, res) => {
+    res.render('main/main', Object.assign({
+        message: `Hello, ${_.get(req, 'user.login', 'User')}!`
+    }, req.commonData));
 };
 
 exports.error404 = (req, res) => res.sendStatus(404);
---
+++ generator/mlabGenerator.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const apiKey = require('../apiKey').apiKey;
+const mLab = require('mongolab-data-api')(apiKey);
+const data = require('./data/data.json');
+const dbName = 'kafkatist';
+
+removeCollections();
+insertCollections();
+
+function removeCollections() {
+    Object.keys(data).forEach(collection => {
+        var options = {
+            database: dbName,
+            collectionName: collection
+        };
+        mLab.deleteDocuments(options, (err, result) => {
+            err ?
+                console.error('There is an error! ' + err) :
+                console.log('Removed ' + result.removed + ' records from ' + collection);
+        });
+    });
+}
+
+function insertCollections() {
+    Object.keys(data).forEach(collection => {
+        var options = {
+            database: dbName,
+            collectionName: collection,
+            documents: data[collection]
+        };
+        mLab.insertDocuments(options, (err, result) => {
+            err ?
+                console.error('There is an error! ' + err) :
+                console.log('Inserted ' + result.n + ' records to ' + collection);
+        });
+    });
+}
