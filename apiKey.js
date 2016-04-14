8dfc4a870f63e85b6ad538ce80265b9caf1e34de
Fedor <mydearfrodo@yandex.ru>
2016-04-14 21:26:11 +0500

    fix(*): heroku keys

--- apiKey.js
+++ apiKey.js
@@ -1,6 +1,6 @@
 const argv = require('minimist')(process.argv.slice(2));
 console.log(argv);
-console.log(process.env.API_KEY);
+console.log(process.argv);
 module.exports = {
     apiKey: process.env.API_KEY
 };
