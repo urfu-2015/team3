82df26831852d14c64613c3563fd6502161c9f60
Anna.Smith <ann_v_smith@bk.ru>
2016-04-25 00:38:16 +0500

    slider.js

--- views/partials/slider/slider.js
+++ views/partials/slider/slider.js
@@ -1,11 +1,10 @@
 require('./slider.styl');
 const Swiper = require('swiper');
-/* eslint no-unused-vars: ["error", { "args": "none" }] */
+/* eslint-disable no-unused-vars*/
 var swiper = new Swiper('.swiper-container', {
     scrollbar: '.swiper-scrollbar',
     scrollbarHide: true,
     slidesPerView: 'auto',
-    // centeredSlides: false,
     spaceBetween: 30,
     grabCursor: true,
     slideToClickedSlide: true,
