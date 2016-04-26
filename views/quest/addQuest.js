e26021767a1a95b331fa80c2f685986de207d826
Anna.Smith <ann_v_smith@bk.ru>
2016-04-26 15:39:09 +0500

    addQuest.js fix

--- views/quest/addQuest.js
+++ views/quest/addQuest.js
@@ -2,14 +2,13 @@ require('./addQuest.styl');
 var width = screen.width;
 var buttons = Array.from(document.querySelectorAll('.btn'));
 if (width <= 320) {
-    buttons.map(function (button) {
+    buttons.forEach(function (button) {
         $(button).addClass("btn-xs");
-    })
-
+    });
 } else if (width <= 375) {
-    buttons.map(function (button) {
+    buttons.forEach(function (button) {
         $(button).addClass("btn-sm");
-    })
+    });
 }
-var form = document.forms.addQuestForm;
-//console.log(form.elements);
+// var form = document.forms.addQuestForm;
+// console.log(form.elements);
